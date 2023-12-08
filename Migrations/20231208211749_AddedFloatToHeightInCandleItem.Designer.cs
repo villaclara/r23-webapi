78b6728060b55829f894fa5dd966c512717f4b7e
villaclara <villaclara1231@gmail.com>
2023-12-08 23:23:27 +0200

    floating number to candle height

--- Migrations/20231208211749_AddedFloatToHeightInCandleItem.Designer.cs
+++ Migrations/20231208211749_AddedFloatToHeightInCandleItem.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Road23.WebAPI.Database;
 
@@ -10,9 +11,11 @@ using Road23.WebAPI.Database;
 namespace Road23.WebAPI.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20231208211749_AddedFloatToHeightInCandleItem")]
+    partial class AddedFloatToHeightInCandleItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.14");
---
+++ Migrations/20231208211749_AddedFloatToHeightInCandleItem.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Road23.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFloatToHeightInCandleItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "HeightCM",
+                table: "Candles",
+                type: "REAL",
+                precision: 8,
+                scale: 1,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "HeightCM",
+                table: "Candles",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "REAL",
+                oldPrecision: 8,
+                oldScale: 1);
+        }
+    }
+}
